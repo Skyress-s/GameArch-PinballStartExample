6bdc69ade46b428db568eb6f4df9983b8f9be4aa
Mathias M <mathmoe64@gmail.com>
2023-09-02 14:33:23 +0200

    feat: Flipper can now start at a custom rotation

--- Assets/Scripts/PinBallGame/Flipper.cs
+++ Assets/Scripts/PinBallGame/Flipper.cs
@@ -5,7 +5,10 @@ public class Flipper : MonoBehaviour {
     [SerializeField] private AnimationCurve animationCurve;
     private float _timer;
     private Rigidbody _rb;
+
+    private Quaternion _startRot;
     private void Awake() {
+        _startRot = transform.rotation;
         _rb = GetComponent<Rigidbody>();
     }
     private void Update() {
@@ -20,6 +23,7 @@ public class Flipper : MonoBehaviour {
         // Instead of rotating object using transform.rotation / transform.localEulerAngles
         // we use rigidbody.MoveRotation. This rotates the object in the physicsstep (Fixed Update) and ensures 
         // other raindrops / pinballs dont clip through and receives velocity!
-        _rb.MoveRotation(Quaternion.Euler(0,0,animationCurve.Evaluate(_timer) * 90f));
+        
+        _rb.MoveRotation(_startRot * Quaternion.Euler(0,0,animationCurve.Evaluate(_timer) * 90f));
     }
 }
